8bdc785f801ac4fc5e28172251904aba0304e046
lightny <ankoshenkov@gmail.com>
2023-06-03 17:25:59 +0300

    trying to trouble shoot

--- app.py
+++ app.py
@@ -3,16 +3,18 @@ from components import name_form, faces_form, sidebar, home_page
 from auth import auth
 from auth import login_page
 
-
-if not "page" in st.session_state:
-    home_page.render()
+if not auth.is_logged_in():
+    login_page.render()
 
 else:
-    page = st.session_state['page']
-    if page == "names":
-        name_form.render()
+    sidebar.render()
+
+    if not "page" in st.session_state:
+        home_page.render()
+
     else:
-        faces_form.render()
-
-
-
+        page = st.session_state['page']
+        if page == "names":
+            name_form.render()
+        else:
+            faces_form.render()
